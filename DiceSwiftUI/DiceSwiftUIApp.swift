c76e82d262f182b6e6ccae4a265abf7e8cb78ce2
lachkar99zakaria <lachkar99zakaria@gmail.com>
2023-03-22 15:57:36 +0000

    Initial Commit

---
+++ DiceSwiftUI/DiceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceSwiftUIApp.swift
+//  DiceSwiftUI
+//
+//  Created by zakaria lachkar on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
